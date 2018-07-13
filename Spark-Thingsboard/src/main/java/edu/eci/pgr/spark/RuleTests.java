ab3b65f02f8ff89b27b5214d114c77b9411d9c9d
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2018-07-12 20:12:03 -0500

    restauración

--- Spark-Thingsboard/src/main/java/edu/eci/pgr/spark/RuleTests.java
+++ Spark-Thingsboard/src/main/java/edu/eci/pgr/spark/RuleTests.java
@@ -30,7 +30,7 @@ public class RuleTests extends  Rule implements Serializable{
         for (String name: data.keySet()){
             String key =name.toString();
             String value = data.get(name).toString();
-            System.out.println(key + " " + value+"hilo1");
+            System.out.println(key + " " + value+"hilo1 ");
         }
     }
 
